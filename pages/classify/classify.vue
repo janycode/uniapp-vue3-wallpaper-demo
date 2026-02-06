dfae8dfc8fc2bc67ee01b914124ee6a48387673c
姜源 <yuan62387@qq.com>
2026-02-06 19:43:30 +0800

    底部选项卡 tabBar:创建页面、设置 tabBar 路由和图标和高亮

---
+++ pages/classify/classify.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="classLayout">
+    <view class="classify">
+      <theme-item v-for="item in 15"></theme-item>
+    </view>
+  </view>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.classify {
+  padding: 30rpx;
+  // 网格布局
+  display: grid;
+  gap: 15rpx;
+  grid-template-columns: repeat(3, 1fr); //每行3个
+}
+</style>

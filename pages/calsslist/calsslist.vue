63628ad3fb365dcaf22130c2855502569f32392b
姜源 <yuan62387@qq.com>
2026-02-07 19:43:58 +0800

    底部安全区全局样式设置，针对数据量大的存本地缓存中

--- pages/calsslist/calsslist.vue
+++ pages/calsslist/calsslist.vue
@@ -5,11 +5,16 @@
         <image src="/common/images/wallpaper/preview1.jpg" mode=""></image>
       </navigator>
     </view>
+    
+    <!-- 底部安全区 -->
+    <view class="safe-area-inset-bottom"></view>
   </view>
 </template>
 
 <script setup>
 import { onLoad, onReachBottom } from '@dcloudio/uni-app'
+import { apiGetClassify } from '../../api/apis'
+import { ref } from 'vue'
 
 //接收 url 携带的参数 :url="`/pages/calsslist/calsslist?id=111&name=jerry`"
 onLoad(e => {
@@ -24,6 +29,14 @@ onReachBottom(() => {
   // pageNum++, 调用方法 func 进行下一页获取和渲染（func 方法中进行展开拼接）
   // 做触底防抖，避免无效网络请求，方案有二：①判断总数与累计数量  ②判断请求结果数据长度
 })
+
+const classList = ref([])
+const getClassList = async () => {
+  let res = await apiGetClassify()
+  classList.value = res.data
+  // 针对数据量比较大，需要传递到跳转的页面时使用本地缓存
+  uni.setStorageSync('storageClassList', classList.value) //离开页面的时候将其清空即可
+}
 
 </script>
 
